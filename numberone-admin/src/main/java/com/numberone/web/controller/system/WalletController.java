f1899cdf22bc3044c22e3a37ef379f5f776611d4
wrf <1426878506@qq.com>
2020-02-12 22:34:54 +0800

    修改

---
+++ numberone-admin/src/main/java/com/numberone/web/controller/system/WalletController.java
@@ -0,0 +1,100 @@
+package com.numberone.web.controller.system;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.numberone.common.page.PageDomain;
+import com.numberone.common.page.TableDataInfo;
+import com.numberone.common.page.TableSupport;
+import com.numberone.common.utils.StringUtils;
+import com.numberone.system.service.TransactionService;
+import com.numberone.system.service.WalletService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/system/wallet")
+@Api("钱包信息")
+public class WalletController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private WalletService walletService;
+    private String prefix = "system/wallet";
+
+    @GetMapping()
+    public String operlog()
+    {
+        return prefix + "/wallet";
+    }
+
+    @GetMapping("/walletInfo")
+    @ApiOperation("/钱包信息")
+    public ModelAndView walletInfo(@RequestParam Map<String,Object> params) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName(prefix+"/walletInfo");
+        model.addObject("params",params);
+        return model;
+    }
+
+    @PostMapping("/list")
+    @ResponseBody
+    @ApiOperation("/钱包信息")
+    public TableDataInfo list(@RequestParam Map<String,Object> params) {
+        startPage();
+        List<Map<String,Object>> list = walletService.getWallet(params);
+        return getDataTable(list);
+    }
+
+    @PostMapping("/cashOut")
+    @ResponseBody
+    @ApiOperation("/提现记录列表")
+    public TableDataInfo cashOut() {
+        startPage();
+        Map<String,Object> params = new HashMap<>();
+        params.put("transactionType","1");
+        List<Map<String,Object>> list = transactionService.getTransaction(params);
+        return getDataTable(list);
+    }
+
+
+    /**
+     * 设置请求分页数据
+     */
+    protected void startPage()
+    {
+        PageDomain pageDomain = TableSupport.buildPageRequest();
+        Integer pageNum = pageDomain.getPageNum();
+        Integer pageSize = pageDomain.getPageSize();
+        if (StringUtils.isNotNull(pageNum) && StringUtils.isNotNull(pageSize))
+        {
+            String orderBy = pageDomain.getOrderBy();
+            PageHelper.startPage(pageNum, pageSize, orderBy);
+        }
+    }
+
+
+    /**
+     * 响应请求分页数据
+     * @return
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    protected TableDataInfo getDataTable(List<?> list)
+    {
+        TableDataInfo rspData = new TableDataInfo();
+        rspData.setCode(0);
+        rspData.setRows(list);
+        rspData.setTotal(new PageInfo(list).getTotal());
+        return rspData;
+    }
+
+}
